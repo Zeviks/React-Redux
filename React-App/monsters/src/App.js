18283f282fab91683b5fbb6b99d61c1db8ea2cad
Natanael Pacheco <natepacheco716@gmail.com>
2021-11-20 18:24:49 -0600

    Card Component

--- React-App/monsters/src/App.js
+++ React-App/monsters/src/App.js
@@ -1,33 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React, { Component } from "react";
-
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Sally",
-          id: "sal",
-        },
-        {
-          name: "Bally",
-          id: "bal",
-        },
-        {
-          name: "Gally",
-          id: "gal",
-        },
-      ],
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 id="{monster.id}">{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
