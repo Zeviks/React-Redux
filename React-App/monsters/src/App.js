9358598ce82c1005c431f441d5fb0bb46dd87e42
Natanael Pacheco <natepacheco716@gmail.com>
2021-11-20 19:14:46 -0600

    SearchField

--- React-App/monsters/src/App.js
+++ React-App/monsters/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: ''
     };
   }
 
@@ -18,6 +19,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type='search' placeholder='search monsters' 
+        onChange={e => this.setState({searchField: e.target.value})}
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     );
